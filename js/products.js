761dab3ff81190cd4c32386a3cf66c2a437606fe
Bruno Castro <110422996+brunovcastro@users.noreply.github.com>
2022-08-29 22:56:51 -0300

    Entrega 1 fetch

--- js/products.js
+++ js/products.js
@@ -1,27 +1,34 @@
-const URL = await fetch ("https://japceibal.github.io/emercado-api/cats_products/101.json").then (response => response.json());
-let auto = document.getElementById("car"); //declaracion de la variable auto para traer la id "car".
+const URL = "https://japceibal.github.io/emercado-api/cats_products/101.json";
+
 
 //funcion que cargará los datos obtenidos:
-function showList(nombre) {
-for (let index = 0; index < nombre.product.length; index++) {
-	auto.innerHTML += `
-    <div class="row">
-                    <div class="col-3">
-                        <img src="${nombre.products[index].image}" alt="auto-gerico" class="img-thumbnail">  
-                    </div>   
-                    <div class="col">
-                        <div class="d-flex w-100 justify-content-between">
-                            <h4 class="mb-1">${nombre.products[index].name}</h4>
-                            <small class="text-muted">${nombre.products[index].soldCount} artículos</small>
-                        </div>
-                        <p class="mb-1">${nombre.products[index].description}</p>
-                    </div>
-                </div>           
-    `
+fetch(URL)
+.then( respuesta => respuesta.json() )  
+.then( data => {
+    const ArrayDatos = data;
+    showinfo(ArrayDatos.products);
+} )
 
-	
+function showinfo(array){
+    array.forEach(element => {
+        var elementHTML = ` <div class="list-group-item list-group-item-action">
+                                <div class="row">
+                                    <div class="col-3">
+                                        <img src="${element.image}" alt="auto-gerico" class="img-thumbnail">  
+                                    </div>   
+                                    <div class="col">
+                                        <div class="d-flex w-100 justify-content-between">
+                                            <div>
+                                                <h4>${element.name}</h4>
+                                                <p>${element.description}</p>
+                                            </div>
+                                            <small>${element.soldCount}</small>
+                                        </div>
+                                    </div>
+                                </div>  
+                            </div> `
+         document.getElementById("info").innerHTML += elementHTML;
+    });
+
 }
 
-}
-
- showList(URL)
